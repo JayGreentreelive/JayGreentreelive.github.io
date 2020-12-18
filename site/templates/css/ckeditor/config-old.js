50031f4effbe92366767f44936bc9557b5410feb
Jay Greentree <JayGreentree@users.noreply.github.com>
2020-12-18 04:18:21 -0500

    fixes

--- site/templates/css/ckeditor/config-old.js
+++ site/templates/css/ckeditor/config-old.js
@@ -4,6 +4,10 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+
 	config.toolbarGroups = [
 		{ name: 'styles', groups: [ 'styles' ] },
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
@@ -20,5 +24,6 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'about', groups: [ 'about' ] }
 	];
 
-	config.removeButtons = 'NewPage,Preview,Templates,Source,PasteFromWord,Replace,Scayt,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Link,Unlink,Anchor,Image,Flash,Table,Smiley,SpecialChar,PageBreak,Iframe,TextColor,BGColor,ShowBlocks,About,SelectAll,Find,NumberedList,BulletedList,Outdent,Indent,Blockquote,CreateDiv,Font,FontSize,Strike,Superscript,Subscript,Cut,Undo,Redo,Copy,Paste,PasteText,Styles,RemoveFormat,BidiLtr,BidiRtl,Language,JustifyLeft,JustifyCenter,JustifyRight,JustifyBlock,Format,Bold,Italic,Underline,Maximize';
+	config.removeButtons = 'NewPage,Preview,Templates,Source,PasteFromWord,Replace,Scayt,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Link,Unlink,Anchor,Image,Flash,Table,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,TextColor,BGColor,Maximize,ShowBlocks,About,SelectAll,Find,NumberedList,BulletedList,Outdent,Indent,Blockquote,CreateDiv,Font,FontSize,Strike,Superscript,Subscript,Cut,Undo,Redo,Copy,Paste,PasteText,Styles,RemoveFormat,JustifyLeft,JustifyCenter,JustifyRight,BidiLtr,BidiRtl,Language';
+
 };
