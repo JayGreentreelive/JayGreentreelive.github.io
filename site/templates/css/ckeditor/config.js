71ed26189f9c52a10abad4f2875d23aa229c6fb5
Jay Greentree <JayGreentree@users.noreply.github.com>
2022-11-14 15:25:21 -0500

    ckeditor plugins added
    Added support for print and email to ckeditor

--- site/templates/css/ckeditor/config.js
+++ site/templates/css/ckeditor/config.js
@@ -1,24 +1,14 @@
-/**
- * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
- */
+CKEDITOR.editorConfig = function( config )
+{
+    config.extraPlugins = 'print,email';
+    config.height = 600;
+    config.resize_enabled = false;
+    config.removePlugins = 'elementspath';
+    config.skin = 'bootstrapck,/site/templates/css/ckeditor/bootstrapck/';
+    config.toolbarCanCollapse = false;
+    config.toolbar =
+    [
+        { name: 'basicstyles', items: [  'Bold', 'Italic', 'Underline',  'HorizontalRule', '-', 'NumberedList', 'BulletedList', '-',    'Email', 'Print'] }
+    ];
 
-CKEDITOR.editorConfig = function( config ) {
-	config.toolbarGroups = [
-		{ name: 'styles', groups: [ 'styles' ] },
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-		{ name: 'forms', groups: [ 'forms' ] },
-		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
-		{ name: 'links', groups: [ 'links' ] },
-		{ name: 'insert', groups: [ 'insert' ] },
-		{ name: 'colors', groups: [ 'colors' ] },
-		{ name: 'tools', groups: [ 'tools' ] },
-		{ name: 'others', groups: [ 'others' ] },
-		{ name: 'about', groups: [ 'about' ] }
-	];
-
-	config.removeButtons = 'NewPage,Preview,Templates,Source,PasteFromWord,Replace,Scayt,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Link,Unlink,Anchor,Image,Flash,Table,Smiley,SpecialChar,PageBreak,Iframe,TextColor,BGColor,ShowBlocks,About,SelectAll,Find,NumberedList,BulletedList,Outdent,Indent,Blockquote,CreateDiv,Font,FontSize,Strike,Superscript,Subscript,Cut,Undo,Redo,Copy,Paste,PasteText,Styles,RemoveFormat,BidiLtr,BidiRtl,Language,JustifyLeft,JustifyCenter,JustifyRight,JustifyBlock,Format,Bold,Italic,Underline,Maximize';
 };
---
+++ site/templates/css/ckeditor/email/plugin.js
@@ -0,0 +1,49 @@
+﻿/**
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+/**
+ * @fileOverview Email Plugin
+ */
+
+CKEDITOR.plugins.add( 'email', {
+
+	icons: 'email,', // %REMOVE_LINE_CORE%
+	hidpi: true, // %REMOVE_LINE_CORE%
+	init: function( editor ) {
+		// Print plugin isn't available in inline mode yet.
+		if ( editor.elementMode == CKEDITOR.ELEMENT_MODE_INLINE )
+			return;
+
+		var pluginName = 'email';
+
+		// Register the command.
+		var command = editor.addCommand( pluginName, CKEDITOR.plugins.email );
+
+		// Register the toolbar button.
+		editor.ui.addButton && editor.ui.addButton( 'Email', {
+			label: 'Email',
+			command: pluginName,
+			toolbar: 'document,50'
+		} );
+	}
+} );
+
+CKEDITOR.plugins.email = {
+	exec: function( editor ) {
+
+	//if($("#email_status").length) $("#email_status").html("");
+
+	$.magnificPopup.open({
+		  items: {
+		     src: '#email-popup',
+		  },
+		  type: 'inline'
+		}, 0);
+
+	},
+	canUndo: false,
+	readOnly: 1,
+	modes: { wysiwyg: 1 }
+};
